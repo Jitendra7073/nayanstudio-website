2a354bf7e8344ca9a8d139bf733b9f78706a3d3b
Jitendra7073 <j9558877073@gmail.com>
2025-04-11 20:22:41 +0530

    inquiry sending mail changed

--- backend/Controller/sendMail.js
+++ backend/Controller/sendMail.js
@@ -16,8 +16,8 @@ const sendMessage = async (
       port: 465,
       secure: true,
       auth: {
-        user: "legendjitesh428@gmail.com", // Your email address
-        pass: "uueymfgwkdulosoe", // App-specific password
+        user: "vesunayanstudio@gmail.com", // Your email address
+        pass: "iyfhefwpucdagbgn", // App-specific password
       },
     });
 
@@ -36,7 +36,7 @@ const sendMessage = async (
     // Email options
     const mailOptions = {
       from: "vesunayanstudio@gmail.com", // Sender's email address
-      to: "legendjitesh428@gmail.com", // Recipient's email address (from input)
+      to: "vesunayanstudio@gmail.com", // Recipient's email address (from input)
       subject: subject, // Subject of the email
       html: emailContent, // HTML content from the EJS template
     };
